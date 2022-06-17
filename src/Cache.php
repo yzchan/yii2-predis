98dde9d6b1cee6a58eb08b30d5c150d124af53dd
chenyuzou <chenyuzou@qq.com>
2022-06-17 15:44:02 +0800

    .

--- src/Cache.php
+++ src/Cache.php
@@ -2,8 +2,95 @@
 
 namespace Yzchan\Yii2Predis;
 
-use yii\base\Component;
+use Predis\Client;
+use yii\di\Instance;
 
-class Cache extends Component
+class Cache extends \yii\caching\Cache
 {
+    /**
+     * @var string|Connection|Client
+     */
+    public $redis = 'redis';
+    public $database = 0;
+
+    public function init()
+    {
+        parent::init();
+        $this->redis = Instance::ensure($this->redis, Connection::className());
+        $this->redis->select($this->database);
+    }
+
+    public function exists($key)
+    {
+        return (bool)$this->redis->exists($this->buildKey($key));
+    }
+
+    public function offsetExists($key)
+    {
+        return $this->exists($key);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getValue($key)
+    {
+        $data = $this->redis->get($key);
+        return $data === null ? false : $data;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getValues($keys)
+    {
+        $response = $this->redis->mget($keys);
+        $result = [];
+        $i = 0;
+        foreach ($keys as $key) {
+            $result[$key] = $response[$i++];
+        }
+
+        return $result;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function setValue($key, $value, $duration)
+    {
+        if ($duration == 0) {
+            return (bool)$this->redis->set($key, $value);
+        } else {
+            return (bool)$this->redis->set($key, $value, 'PX', (int)($duration * 1000));
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function addValue($key, $value, $duration)
+    {
+        if ($duration == 0) {
+            return (bool)$this->redis->set($key, $value, 'NX');
+        } else {
+            return (bool)$this->redis->set($key, $value, 'PX', (int)($duration * 1000), 'NX');
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function deleteValue($key)
+    {
+        return (bool)$this->redis->del($key);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function flushValues()
+    {
+        return $this->redis->flushdb();
+    }
 }
--- src/Session.php
+++ src/Session.php
@@ -2,8 +2,48 @@
 
 namespace Yzchan\Yii2Predis;
 
-use yii\base\Component;
+use yii\di\Instance;
 
-class Session extends Component
+class Session extends \yii\web\Session
 {
+    public $redis = 'redis';
+
+    public $keyPrefix;
+
+    public function init()
+    {
+        $this->redis = Instance::ensure($this->redis, Connection::className());
+        if ($this->keyPrefix === null) {
+            $this->keyPrefix = substr(md5(\Yii::$app->id), 0, 5);
+        }
+        parent::init();
+    }
+
+    public function getUseCustomStorage()
+    {
+        return true;
+    }
+
+    public function readSession($id)
+    {
+        $data = $this->redis->get($this->calculateKey($id));
+
+        return $data === false || $data === null ? '' : $data;
+    }
+
+    public function writeSession($id, $data)
+    {
+        return (bool)$this->redis->set($this->calculateKey($id), $data, 'EX', $this->getTimeout());
+    }
+
+    public function destroySession($id)
+    {
+        $this->redis->del($this->calculateKey($id));
+        return true;
+    }
+
+    protected function calculateKey($id)
+    {
+        return $this->keyPrefix . md5(serialize($id));
+    }
 }
